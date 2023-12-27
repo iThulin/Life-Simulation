d2b65ef1ce19818c3416ebb83efac3fc4f2962f6
Ian <ian.thulin@icloud.com>
2023-12-27 16:38:12 -0700

    Added coulombs law based attraction/repulsion

--- particle.py
+++ particle.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Particle:
     def __init__(self, color, x_boundary, y_boundary):
@@ -12,6 +13,8 @@ class Particle:
         self.accel_y = random.uniform(-0.00001, 0.00001)
         self.color = color
         self.size = random.randint(0, 10)
+        self.mass = math.sqrt(self.size)
+        self.charge = 1
 
     def move(self):
 
@@ -30,3 +33,38 @@ class Particle:
             #self.y = -self.y if self.y < 0 else 2 * self.size - self.y
             self.vel_y = -self.vel_y
             #self.accel_y = 0
+
+    def calculate_force(self, other_x, other_y, other_charge):
+        dist_x = self.x - other_x
+        dist_y = self.y - other_y
+
+        # If particles are at the same point return no force
+        if dist_x == dist_y == 0:
+            return 0, 0, 0 # No force in this case
+
+        # Force = k * q1 * q2 / r_sq
+        k = 8.99e9
+        q_1 = self.charge
+        q_2 = other_charge
+        r_sq = math.sqrt(dist_x ** 2 + dist_y ** 2)
+
+        # Calculate the force magnitude
+        F = (k * q_1 * q_2) / r_sq
+
+        # Calculate the force components along the x and y axes
+        force_x = F * (dist_x / r_sq)
+        force_y = F * (dist_y / r_sq)
+
+        # Determine direction of force
+        if (q_1 > 0 and q_2 > 0) or (q_1 < 0 and q_2 < 0):
+            force_x *= -1
+            force_y *+ -1
+
+        return force_x, force_y, F # Return for components and magnitude
+    
+    def apply_force(self, other):
+        new_force = self.calculate_force(other.x, other.y, other.charge)
+
+        self.accel_x += new_force[0] / self.mass
+        self.accel_y += new_force[1] / self.mass
+        
