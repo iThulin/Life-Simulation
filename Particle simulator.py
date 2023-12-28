a6c3057b5fae68ca773b1d2ac9c7e375246f44f0
Ian <ian.thulin@icloud.com>
2023-12-28 16:51:30 -0700

    Added interactions between particles

--- Particle simulator.py
+++ Particle simulator.py
@@ -6,6 +6,7 @@ import logging
 import math
 from particle import Particle
 from datetime import datetime
+import decimal as dec
 
 '''
 DEBUG   Detailed information, typically of interest only when diagnosing problems.
@@ -31,8 +32,12 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Define particle parameters
-STARTING_PARTICLES = 1000
+STARTING_PARTICLES = 10
 STARTING_COLOR = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+STARTING_CHARGE = dec.Decimal(1e-8)
+SECOND_PARTICLES = 20
+SECOND_COLOR = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+SECOND_CHARGE = dec.Decimal(-1e-8)
 
 # Initialize the game environment
 pygame.init()
@@ -54,36 +59,49 @@ def is_touching(c1, c2):
 def handle_collisions(particles):
     log_objects(particles)
 
-    print("\nSTART\n")
+    #print("\nSTART\n")
 
-    #is_touching()
+    for particle in particles:
+        for second_particle in particles:
+            particle.apply_force(second_particle)
+            #second_particle.apply_force(particle)
 
-    print("\nEND\n")
+    #print("\nEND\n")
 
     return particles
 
 def update_sim(particles):
+    #handle_collisions(particles)
+
     for particle in particles:
-        particle.move()
-        pygame.draw.circle(display, particle.color, (particle.x, particle.y), particle.size)
+        for second_particle in particles:
+            particle.apply_force(second_particle)
+            particle.move()
+            pygame.draw.circle(display, particle.color, (float(particle.x), float(particle.y)), particle.size)
 
     pygame.display.update()
     clock.tick(FPS)
     return particles
 
-def create_particles(particles, num_particles, color, WIDTH, HEIGHT):
+def create_particles(particles, num_particles, color, WIDTH, HEIGHT, CHARGE):
+    init_num_x = dec.Decimal(WIDTH / 2)
+    init_num_y = dec.Decimal(HEIGHT / 2)
 
     if len(particles) == 0:
         for _ in range(0, STARTING_PARTICLES):
-            particles.append(Particle(STARTING_COLOR,WIDTH, HEIGHT))
+            init_num_x += dec.Decimal(5)
+            init_num_y += dec.Decimal(5)
+            particles.append(Particle(color, init_x= init_num_x, init_y= init_num_y, x_boundary=WIDTH, y_boundary=HEIGHT, charge=CHARGE))
         for entity in particles:
-            pygame.draw.circle(display, color, (int(entity.x), int(entity.y)), entity.size)
+            pygame.draw.circle(display, color, (float(entity.x), float(entity.y)), entity.size)
             logging.info('Initial particle %s', entity.color)
     else:
         for _ in range (0, num_particles - len(particles)):
-            new_particle = Particle(color,WIDTH, HEIGHT)
+            init_num_x += dec.Decimal(5)
+            init_num_y += dec.Decimal(5)
+            new_particle = Particle(color, init_x= init_num_x, init_y= init_num_y, x_boundary=WIDTH, y_boundary=HEIGHT, charge=CHARGE)
             particles.append(new_particle)
-            pygame.draw.circle(display, new_particle.color, (int(new_particle.x), int(new_particle.y)), new_particle.size)
+            pygame.draw.circle(display, new_particle.color, (float(new_particle.x), float(new_particle.y)), new_particle.size)
             logging.info('Added particle %s', new_particle.color)
     return particles
 
@@ -92,6 +110,7 @@ def main():
     # Game loop begins
     running = True
     particle_spawned = False
+    second_particle_spawned = False
     particles = []
     while running: 
         display.fill(BLACK)
@@ -104,8 +123,18 @@ def main():
                             STARTING_PARTICLES,
                             color=STARTING_COLOR,
                             WIDTH=WIDTH,
-                            HEIGHT=HEIGHT)
+                            HEIGHT=HEIGHT,
+                            CHARGE= STARTING_CHARGE)
             particle_spawned = True
+        if second_particle_spawned == False:
+            create_particles(particles,
+                             SECOND_PARTICLES,
+                             color=SECOND_COLOR,
+                             WIDTH= WIDTH,
+                             HEIGHT= HEIGHT,
+                             CHARGE= SECOND_CHARGE)
+            second_particle_spawned = True
+
         
         update_sim(particles)
 
--- particle.py
+++ particle.py
@@ -1,38 +1,41 @@
 import random
 import math
+import decimal as dec
 
 class Particle:
-    def __init__(self, color, x_boundary, y_boundary):
+    def __init__(self, color, init_x, init_y, x_boundary, y_boundary, charge):
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
-        self.x = random.randint(0, x_boundary)
-        self.y = random.randint(0, y_boundary)
-        self.vel_x = random.uniform(-0.001, 0.001)
-        self.vel_y = random.uniform(-0.001, 0.001)
-        self.accel_x = random.uniform(-0.00001, 0.00001)
-        self.accel_y = random.uniform(-0.00001, 0.00001)
+        self.x = dec.Decimal(random.uniform(0, x_boundary))
+        self.y = dec.Decimal(random.uniform(0, y_boundary))
+        self.vel_x = dec.Decimal(0)
+        self.vel_y = dec.Decimal(0)
+        self.accel_x = dec.Decimal(0)
+        self.accel_y = dec.Decimal(0)
         self.color = color
-        self.size = random.randint(0, 10)
-        self.mass = math.sqrt(self.size)
-        self.charge = 1
+        self.size = random.randint(1, 5)
+        self.mass = dec.Decimal(self.size).sqrt()
+        self.charge = charge
 
     def move(self):
 
         # self.dt is one time step in the movement function.
-        self.vel_x += self.accel_x #* self.dt
-        self.vel_y += self.accel_y #* self.dt
-        self.x += self.x * self.vel_x
-        self.y += self.y * self.vel_y
+        self.vel_x += self.accel_x 
+        self.vel_y += self.accel_y 
+        self.x += dec.Decimal(self.x) * dec.Decimal(self.vel_x)
+        self.y += dec.Decimal(self.y) * dec.Decimal(self.vel_y)
 
         # Handle collisions with the walls of the "box"
-        if self.x < 0  or self.x > self.x_boundary - self.size:
-            #self.x = -self.x if self.x < 0 else 2 * self.size - self.x
+        if self.x < 0 + self.size:
             self.vel_x = -self.vel_x
             #self.accel_x = 0
-        if self.y < 0 or self.y > self.y_boundary - self.size:
-            #self.y = -self.y if self.y < 0 else 2 * self.size - self.y
+        if self.x > self.x_boundary - self.size:
+            self.vel_x = -self.vel_x
+        if self.y < 0 + self.size:
             self.vel_y = -self.vel_y
             #self.accel_y = 0
+        if self.y > self.y_boundary - self.size:
+            self.vel_y = -self.vel_y
 
     def calculate_force(self, other_x, other_y, other_charge):
         dist_x = self.x - other_x
@@ -42,11 +45,15 @@ class Particle:
         if dist_x == dist_y == 0:
             return 0, 0, 0 # No force in this case
 
+        # If particles are too far apart break the calculation
+        if dist_x + dist_y > 400:
+            return 0, 0, 0
+
         # Force = k * q1 * q2 / r_sq
-        k = 8.99e9
+        k = dec.Decimal(8.99e9)
         q_1 = self.charge
         q_2 = other_charge
-        r_sq = math.sqrt(dist_x ** 2 + dist_y ** 2)
+        r_sq = dec.Decimal(dist_x ** 2 + dist_y ** 2).sqrt()
 
         # Calculate the force magnitude
         F = (k * q_1 * q_2) / r_sq
@@ -67,4 +74,7 @@ class Particle:
 
         self.accel_x += new_force[0] / self.mass
         self.accel_y += new_force[1] / self.mass
+
+        #print(f"Accel_x: {self.accel_x}")
+        #print(f"Accel_y: {self.accel_y}")
         
